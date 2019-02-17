dd4507bdb3a4a04bd66f6de7969596828faaf50b
Hannah Grandine <hannahgrandine@gmail.com>
2019-02-17 10:37:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import psycopg2
 import json
 
-app = Flask(__name__)
+app = Flask(__name__) #create the Flask app
 
-''@app.route('/')
+when = "global"
+data = "global"
+
 def connect():
     ''""" Connect to the PostgreSQL database server """
     localhost = "findmyspot.cmpdtcyalbuc.us-west-2.rds.amazonaws.com"
@@ -24,7 +26,7 @@ def connect():
         cur = conn.cursor()
 
         # execute a statement
-        print('Top 20 spots near Capitol Hill: ')
+        print('Top 10 spots near Capitol Hill: ')
         cur.execute("SELECT row_to_json(top10spots(17));")
 
         # display the PostgreSQL database server version
@@ -32,18 +34,39 @@ def connect():
         coordz = db_version['geotag'].split("POINT (", 2)[1].split(" ")
         lat = coordz[0]
         long = coordz[1].split(")")[0]
-        #return render_template("index.html", name=top20)
-        return render_template("Map.html", lat=lat, long=long)
+        coords = [lat, long]
+        # return render_template("index.html", name=top20)
+        return coords
         cur.close()
-
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
 
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/", methods=['GET', 'POST']) #allow both GET and POST requests
+def home():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        when = request.form.get('when')
+
+        data = "avaliable"
+
+        coords = connect()
+
+        return render_template("Map.html", lat=coords[0], long=coords[1])
+
+        ''''''''''
+                  <h1>Estimated parking time: {}</h1>
+                  <h1>Parking spaces are: {}</h1>.format(when, coords)'''
+
+    return '''<form method="POST">
+                  Hello!<br>
+                  When: <input type="text" name="when"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#http://127.0.0.1:5000/
