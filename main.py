18c4e024ac3d3dd4b4e960b261086faa3ea5717e
dingx170 <36064335+dingx170@users.noreply.github.com>
2019-02-17 10:19:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ app = Flask(__name__) #create the Flask app
 when = "global"
 data = "global"
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST']) #allow both GET and POST requests
 def home():
-    return "Hello, World!"
-
-@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
-def form_example():
     if request.method == 'POST':  #this block is only entered when the form is submitted
         when = request.form.get('when')
 
         if when > 5:
             data = "avaliable"
 
-
-        return '''<h1>Estimated parking time: {}</h1>
+        return '''
+                  <h1>Estimated parking time: {}</h1>
                   <h1>Parking spaces are: {}</h1>'''.format(when, data)
 
     return '''<form method="POST">
+                  Hello!<br>
                   When: <input type="text" name="when"><br>
                   <input type="submit" value="Submit"><br>
               </form>'''
@@ -31,5 +28,3 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 #http://127.0.0.1:5000/
-#http://127.0.0.1:5000/form-example
-
