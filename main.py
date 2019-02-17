b317ca4853733fd88b5642353e1593d0b9f266a0
dingx170 <36064335+dingx170@users.noreply.github.com>
2019-02-17 09:43:24 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+
+app = Flask(__name__) #create the Flask app
+
+when = "global"
+data = "global"
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        when = request.form.get('when')
+
+        if when > 5:
+            data = "avaliable"
+
+
+        return '''<h1>Estimated parking time: {}</h1>
+                  <h1>Parking spaces are: {}</h1>'''.format(when, data)
+
+    return '''<form method="POST">
+                  When: <input type="text" name="when"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#http://127.0.0.1:5000/
+#http://127.0.0.1:5000/form-example
+
